60979bdee852ab2c079a7df21eb2d902531b4426
WumiDev <wumi2021@gmail.com>
2023-04-03 21:47:17 +0000

    project completed

--- scripts/mint-nft.mjs
+++ scripts/mint-nft.mjs
@@ -1,7 +1,13 @@
-const CONTRACT_ADDRESS = "PASTE_CONTRACT_ADDRESS";
-const META_DATA_URL = "PASTE_URL_HERE";
+const CONTRACT_ADDRESS = "0x53bf6a2c5cAd8AD2f9fff3A44B056db3051a0866";
+const META_DATA_URL = "ipfs://bafyreifyhuhvso6gjn7i7e2dida7jcrv7nbqq22446vi4j4gulci4f37gu/metadata.json";
 
-// paste function here
+// Function to mint NFT
+async function mintNFT(contractAddress, metaDataURL) {
+  const ExampleNFT = await ethers.getContractFactory("ExampleNFT");
+  const [owner] = await ethers.getSigners();
+  await ExampleNFT.attach(contractAddress).mintNFT(owner.address, metaDataURL);
+  console.log("NFT minted to: ", owner.address);
+ }
 
 mintNFT(CONTRACT_ADDRESS, META_DATA_URL)
   .then(() => process.exit(0))
--- scripts/store-asset.mjs
+++ scripts/store-asset.mjs
@@ -5,7 +5,17 @@ dotenv.config();
 
 const API_KEY = process.env.NFT_STORAGE_API_KEY;
 
-// Paste function here
+// Function to store asset on NFT.Storage
+async function storeAsset() {
+  const client = new NFTStorage({ token: API_KEY });
+  const file = await fs.promises.readFile("./assets/myNFTimage.png");
+  const metadata = await client.store({
+   name: "WumiDev",
+   description: "I love Filecoin!",
+   image: new File([file], "myNFTimage.png", { type: "image/png" }),
+  });
+  console.log("Metadata stored on Filecoin and IPFS with URL:", metadata.url);
+ }
 
 storeAsset()
   .then(() => process.exit(0))
